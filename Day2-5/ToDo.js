365d14b9039428a6fe55c51f8cb4d0a68e90cdc5
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-27 12:30:46 +1300

    Check if ToDo.js is imported correctly

---
+++ Day2-5/ToDo.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import {View, Text,  StyleSheet} from 'react-native';
+
+export default class ToDo extends Component{
+  render(){
+    return(
+      //11. Creat ToDo.js checks if they works
+      <Text>Hello I'm ToDo List</Text>
+    );
+  }
+  
+}
+
+const style= StyleSheet.create({
+  
+})
